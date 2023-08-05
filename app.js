a8a4f9f8d677e335642a3eb18a7ec46a318f66f3
JustinV <127573153+JustVDev@users.noreply.github.com>
2023-08-05 18:56:34 -0500

    Finish application

--- app.js
+++ app.js
@@ -3,60 +3,67 @@ const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
 
-
-let num1 = null, 
-num2 = null,
-operator = null;
-
 const display = document.querySelector('#display');
-let displayValue = 0;
-// +btn.id[1] //change to a num from a string
+let num1 = null;
+let num2 = null;
+let operator = null;
 
 const btns = [...document.querySelectorAll('button')];
 btns.forEach(btn => btn.addEventListener('click', () => {
     //if it is a number button
-    if(btn.id.startsWith('n')){
-        if(displayValue === 0){
-            displayValue = btn.id[1];
+    if (btn.id.startsWith('n')) {
+        if(operator === 'Divide' && btn.id === 'n0'){
+            reset()
+            display.textContent = 'Cannot divide by 0, please clear'
+
+        }
+        else if (display.textContent === "0") {
             display.textContent = btn.id[1];
         } else {
-            displayValue += btn.id[1];
             display.textContent += btn.id[1];
         }
-        
     }
+
     //if it is an operator button
-    if(btn.id.startsWith('o')){
-        if(!num1){
-            num1 = +displayValue;
-            operator = btn.id.slice(1);
-            displayValue = 0;
-            display.textContent = displayValue;
+    if (btn.id.startsWith('o')) {
+        if (!num1) {
+            num1 = +display.textContent;
+            display.textContent = 0;
 
+            operator = btn.id.slice(1);
         } else {
-            num2 = +displayValue;
-            let answer = operation(num1, operator, num2);
-            displayValue = answer;
-            display.textContent = answer;
+            num2 = +display.textContent;
+            num1 = operation(num1, operator, num2);
+            display.textContent = 0;
+            operator = btn.id.slice(1);
+            num2 = null; // Reset num2 for the next operation
         }
-    if(btn.id.startsWith('e')){
-        num2 = +displayValue;
-        let answer = operation(num1, operator, num2);
-        displayValue = answer;
-        display.textContent = answer;
     }
 
-        
+    //if it is the equal button
+    if (btn.id.startsWith('e')) {
+        if (!num2) {
+            num2 = +display.textContent;
+        }
+        const answer = operation(num1, operator, num2);
+        display.textContent = answer;
+        num1 = answer; // Set num1 to the answer in case the user wants to continue with another operation
+        num2 = null; // Reset num2 for the next operation
+        operator = null; // Reset the operator
+    }
+    if(btn.id === 'clear'){
+        reset()
 
-        
+
     }
 }));
 
-
-
-
-
-
+function reset() {
+    num1 = null;
+    num2 = null;
+    operator = null;
+    display.textContent = 0;
+}
 
 function operation(num1, operator, num2) {
     switch(operator){
@@ -75,5 +82,3 @@ function operation(num1, operator, num2) {
     }
 }
 
-
-let result = add(num1,num2);
